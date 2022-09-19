4d5f71f8472ab78931de33b89d19eaae6dbd0c3a
mertovich <88425310+mertovich@users.noreply.github.com>
2022-09-19 16:47:19 +0300

    add(MyList)

---
+++ pages/mylist.tsx
@@ -0,0 +1,11 @@
+import { Text } from "@chakra-ui/react";
+import PageLayout from "../components/layouts/PageLayout";
+const MyList = () => {
+  return (
+    <PageLayout pageTitle="my list">
+      <Text>My list page</Text>
+    </PageLayout>
+  );
+};
+
+export default MyList;
