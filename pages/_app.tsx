dd9476c28b4823622d051b146e69dbaef79dfba1
mertovich <88425310+mertovich@users.noreply.github.com>
2022-09-19 16:47:02 +0300

    add(Navbar)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,13 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import Layout from "../components/layouts/Layout";
+import Navbar from "../components/Navbar";
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <ChakraProvider>
       <Layout>
+        <Navbar />
         <Component key={router.route} {...pageProps} />
       </Layout>
     </ChakraProvider>
