5852c2ec424c7d69d0f84168cb5be33d22230b93
mertovich <88425310+mertovich@users.noreply.github.com>
2022-09-22 10:22:03 +0300

    update(myList): name update

--- pages/mywatchlist.tsx
+++ pages/mywatchlist.tsx
@@ -1,11 +1,11 @@
 import { Text } from "@chakra-ui/react";
 import PageLayout from "../components/layouts/PageLayout";
-const MyList = () => {
+const MyWatchlist = () => {
   return (
-    <PageLayout pageTitle="my list">
-      <Text>My list page</Text>
+    <PageLayout pageTitle="My watchlist">
+      <Text>My watchlist page</Text>
     </PageLayout>
   );
 };
 
-export default MyList;
+export default MyWatchlist;
