797ce1871123ca1b1c7f234561a14a71c9f9701c
mertovich <88425310+mertovich@users.noreply.github.com>
2022-09-23 09:29:23 +0300

    update(Navbar): Link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,9 +3,9 @@ import { HStack, Link } from "@chakra-ui/react";
 const Navbar = () => {
   return (
     <HStack justifyContent="center" marginTop="2%">
-      <Link href="/">Home</Link>
-      <Link href="/mywatchlist">My watchlist </Link>
-      <Link href="/watchedlist">Watched list</Link>
+      <Link href="/film-walet">Home</Link>
+      <Link href="/film-walet/mywatchlist">My watchlist </Link>
+      <Link href="/film-walet/watchedlist">Watched list</Link>
     </HStack>
   );
 };
