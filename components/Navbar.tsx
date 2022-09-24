255ee6c0a6acadd11d4595c7371986cc8ad5061a
mertovich <88425310+mertovich@users.noreply.github.com>
2022-09-24 11:15:23 +0300

    update(Navbar)

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,42 @@
-import { HStack, Link } from "@chakra-ui/react";
+import { Box, HStack, Link } from "@chakra-ui/react";
 
 const Navbar = () => {
   return (
-    <HStack justifyContent="center" paddingTop="2%">
-      <Link href="/film-wallet">Home</Link>
-      <Link href="/film-wallet/mywatchlist">My watchlist </Link>
-      <Link href="/film-wallet/mywatchedlist">My Watched list</Link>
-    </HStack>
+    <Box paddingBottom="1%" position="relative">
+      <HStack
+        position="fixed"
+        top="0"
+        w="100%"
+        backgroundColor="#392f5a"
+        justifyContent="center"
+        paddingTop="1%"
+        paddingBottom="1%"
+        boxShadow="xl"
+        zIndex="6"
+      >
+        <Link
+          href="/film-wallet"
+          _hover={{ color: "#F4D06F", textDecoration: "setline" }}
+          transitionDuration="0.5s"
+        >
+          Home
+        </Link>
+        <Link
+          href="/film-wallet/mywatchlist"
+          _hover={{ color: "#F4D06F" }}
+          transitionDuration="0.5s"
+        >
+          My watchlist{" "}
+        </Link>
+        <Link
+          href="/film-wallet/mywatchedlist"
+          _hover={{ color: "#F4D06F" }}
+          transitionDuration="0.5s"
+        >
+          My Watched list
+        </Link>
+      </HStack>
+    </Box>
   );
 };
 
